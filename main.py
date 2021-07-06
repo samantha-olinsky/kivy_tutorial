55534f43cbd8f65ca58eb9603db086eff9c32d42
samantha-olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2021-07-06 17:17:06 -0400

    change label to image

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        img = Image(source='purple.png',
+                    size_hint=(1, .5),
+                    pos_hint={'center_x': .5, 'center_y': .5})
 
-        return label
+        return img
 
 if __name__ == '__main__':
     app = MainApp()
